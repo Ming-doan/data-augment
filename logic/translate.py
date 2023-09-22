3374bc221f72a41d1aefadf3c83c44c49e13ac97
Ming Doan <78459523+Ming-doan@users.noreply.github.com>
2023-09-22 13:11:13 +0700

    merge: Cutout, Translate (#4)
    * Fixed cutout and translate
    
    * Feature: Cutout and Translate
    
    ---------
    
    Co-authored-by: khoaND1307 <khoa130704@gmail.com>

--- logic/translate.py
+++ logic/translate.py
@@ -8,10 +8,31 @@ from ._utils import *
 class Translate(Method):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.frame_cutter = False
+        self.frame_cutter = True
+        self.randomFn = DistRand(range=(0.1, 0.2, 0.01))
 
     def transform(self, images, bboxs, width, height):
         # Define your algorithm here 👇
 
+        transX = []
+        transY = []
+
+        for image in images:
+            tranX = int(self.randomFn.rand() * image.shape[1])
+            transX.append(tranX)
+            tranY = int(self.randomFn.rand() * image.shape[0])
+            transY.append(tranY)
+
+        new_bboxs = []
+
+        for i, bbox in enumerate(bboxs):
+            x, y, w, h = bbox
+            print(x, y, w, h)
+            x -= transX[i]
+            y -= transY[i]
+
+            print(list((x, y, w, h)))
+            new_bboxs.append(list((x, y, w, h)))
+
         # Define your algorithm here 👆
-        return images, bboxs
+        return images, new_bboxs
