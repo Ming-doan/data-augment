9ad3bc2c2ed58904e0ca35617cc8668094aae58a
khoaND1307 <khoa130704@gmail.com>
2023-09-22 12:03:18 +0700

    Fixed cutout and translate

--- logic/cutout.py
+++ logic/cutout.py
@@ -8,10 +8,30 @@ from ._utils import *
 class CutOut(Method):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.frame_cutter = False
+        self.frame_cutter = True
+        self.randomLength = DistRand(range=(1, 2, 0.1))
+        self.randomColorVariable = DistRand(range=(0, 255))
 
     def transform(self, images, bboxs, width, height):
         # Define your algorithm here 👇
+        newImages = []
+        for i, image in enumerate(images):
+            x, y, w, h = bboxs[i]
+
+            # Get cutout box coordinates
+            cutX = int(w * self.randomLength.rand())
+            cutY = int(h * self.randomLength.rand())
+            length = int(w * self.randomLength.rand())
+
+            # Get random color for cutout box
+            rVal = int(self.randomColorVariable.rand())
+            gVal = int(self.randomColorVariable.rand())
+            bVal = int(self.randomColorVariable.rand())
+
+            # Draw cutout box onto picture
+            image = cv.rectangle(image, (cutX, cutY), (cutX + length, cutY + length), (rVal, gVal, bVal), -1)
+
+            newImages.append(image)
 
         # Define your algorithm here 👆
-        return images, bboxs
+        return newImages, bboxs
--- logic/translate.py
+++ logic/translate.py
@@ -9,9 +9,28 @@ class Translate(Method):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.frame_cutter = False
+        self.randomFn = DistRand(range=(0, 1, 0.1))
 
     def transform(self, images, bboxs, width, height):
         # Define your algorithm here 👇
 
+        transX = []
+        transY = []
+
+        for image in images:
+            tranX = self.randomFn.rand() * image.shape[1]
+            transX.append(tranX)
+            tranY = self.randomFn.rand() * image.shape[0]
+            transY.append(tranY)
+
+        new_bboxs = []
+
+        for i, bbox in enumerate(bboxs):
+            x, y, w, h = bbox
+            x += transX[i]
+            y += transY[i]
+
+            new_bboxs.append([x, y, w, h])
+
         # Define your algorithm here 👆
-        return images, bboxs
+        return images, new_bboxs
