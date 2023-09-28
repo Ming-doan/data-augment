b667fd8dfd7001ee3f58e587ae60853412970d23
Ming-doan <quangminh57dng@gmail.com>
2023-09-28 19:12:23 +0700

    chore: Fix Translate + Tiny features

--- logic/translate.py
+++ logic/translate.py
@@ -9,7 +9,7 @@ class Translate(Method):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.frame_cutter = True
-        self.randomFn = DistRand(range=(0.1, 0.2, 0.01))
+        self.randomFn = DistRand(range=(-0.2, 0.2, 0.01), mode='norm')
 
     def transform(self, images, bboxs, width, height):
         # Define your algorithm here 👇
@@ -18,17 +18,17 @@ class Translate(Method):
         transY = []
 
         for image in images:
-            tranX = int(self.randomFn.rand() * image.shape[1])
+            tranX = int(1 - self.randomFn.rand() * image.shape[1])
             transX.append(tranX)
-            tranY = int(self.randomFn.rand() * image.shape[0])
+            tranY = int(1 - self.randomFn.rand() * image.shape[0])
             transY.append(tranY)
 
         new_bboxs = []
 
         for i, bbox in enumerate(bboxs):
             x, y, w, h = bbox
-            x -= transX[i]
-            y -= transY[i]
+            x += transX[i]
+            y += transY[i]
 
             new_bboxs.append(list((x, y, w, h)))
 
